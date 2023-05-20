452dce970d6492392afd7241827e3d9d659451e7
clonsai <mushiqingqian@gmail.com>
2023-05-20 19:38:35 +0800

    禁止陌生人使用自己的api key

--- app/utils/env.ts
+++ app/utils/env.ts
@@ -5,7 +5,7 @@ export const env = {
   OPENAI_API_KEY_ALIAS: process.env.OPENAI_API_KEY_ALIAS ?? undefined,
 
   /** 禁止陌生人通过他自己的 apiKey 访问 */
-  CHATGPT_NEXT_DISABLE_PUBLIC: process.env.CHATGPT_NEXT_DISABLE_PUBLIC ?? 'false',
+  CHATGPT_NEXT_DISABLE_PUBLIC: process.env.CHATGPT_NEXT_DISABLE_PUBLIC ?? 'yes',
 
   /** 配置 API 请求的 host（包含端口） */
   CHATGPT_NEXT_API_HOST: process.env.CHATGPT_NEXT_API_HOST ?? 'api.openai.com',
